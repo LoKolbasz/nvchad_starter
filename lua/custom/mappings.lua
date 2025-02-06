93988eaa416cfbba2e6380896a84cc7444fb3bd9
Battyányi Dániel <86350313+LoKolbasz@users.noreply.github.com>
2025-02-06 04:02:45 +0100

    Moved to from old nvchad keymapping scheme to which-key

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -1,56 +1,102 @@
-local M = {}
+local map = require("which-key").add
 
-M.dap = {
-    plugin = true,
-    n = {
-        ["<leader>db"] = {"<cmd>DapToggleBreakpoint <CR>", "Add breakpoint at line"},
-        ["<leader>dr"] = {"<cmd>DapContinue <CR>", "Start or continue the debugger"},
-        ["<leader>dg"] = {"<cmd> :lua os.execute(\"cd build && cmake -S ../ -B ./ -DCMAKE_BUILD_TYPE=debug -DCMAKE_EXPORT_COMPILE_COMMANDS=1\") <CR>", "Generates a compile_commands.json for ale supported tools"}
-    }
+map {
+  { "<leader> d", group = "Debug" },
+  {
+    mode = { "n" },
+    { "<leader>db", "<cmd>DapToggleBreakpoint <CR>", desc = "Add breakpoint at line" },
+    { "<leader>dr", "<cmd>DapContinue <CR>", desc = "Start or continue the debugger" },
+    {
+      "<leader>dg",
+      '<cmd> :lua os.execute("cd build && cmake -S ../ -B ./ -DCMAKE_BUILD_TYPE=debug -DCMAKE_EXPORT_COMPILE_COMMANDS=1") <CR>',
+      desc = "Generates a compile_commands.json for ale supported tools",
+    },
+  },
 }
 
-M.dap_python = {
-    plugin = true,
-    n = {
-        ["<leader>dpr"] = {
-            function()
-                require("dap-python").test_method()
-            end
-        }
-    }
-}
-M.general = {
-    n = {
-        ["<C-h>"] = {"<cmd> TmuxNavigateLeft<CR>", "window left"},
-        ["<C-l>"] = {"<cmd> TmuxNavigateRight<CR>", "window right"},
-        ["<C-j>"] = {"<cmd> TmuxNavigateDown<CR>", "window down"},
-        ["<C-k>"] = {"<cmd> TmuxNavigateUp<CR>", "window up"},
-        ["<leader>gg"] = {"<cmd>LazyGit <CR>", "Lazygit"},
-        -- ["<leader>tt"] =  {function () require("trouble").toggle() end, "Toggle LSP diagnostics"},
-        -- ["<leader>tw"] =  {function () require("trouble").toggle("workspace_diagnostics") end, "LSP worksace diagnostics"},
-        -- ["<leader>td"] =  {function () require("trouble").toggle("diagnostics_buffer") end, "LSP document diagnostics"},
-        -- ["<leader>tq"] =  {function () require("trouble").toggle("quickfix") end, "LSP quickfix items"},
-        -- ["<leader>tl"] =  {function () require("trouble").toggle("loclist") end, "LSP location list items"},
-        -- ["<leader>gR"] =  {function () require("trouble").toggle("lsp_references") end, "LSP references of word under cursor"},
-        ["<leader>pvs"] = {function () require("swenv.api").get_current_venv() end, "Show the current venv"},
-        ["<leader>pvp"] = {function () require("swenv.api").pick_venv() end, "Pick a venv"},
-        ["<leader>trf"] = {function () require("neotest").run.run(vim.fn.expand("%")) end, "Run tests in file"},
-        ["<leader>trn"] = {function () require("neotest").run.run() end, "Run nearest test"},
-        ["<leader>ls"] = {function () vim.lsp.buf.signature_help() end, "LSP signature help"},
-    ["<leader>lf"] = {function() vim.diagnostic.open_float { border = "rounded" } end, "Floating diagnostic"},
-    ["<leader>ra"] = {
+map {
+  {
+    mode = { "n" },
+    {
+      "<leader>dpr",
       function()
-        require("nvchad.renamer").open()
+        require("dap-python").test_method()
       end,
-      "LSP rename",
     },
-
-    ["<leader>ca"] = {
-      function()
-        vim.lsp.buf.code_action()
-      end,
-      "LSP code action",
-    },
-    },
+  },
 }
-return M
+map {
+  {
+    mode = { "n" },
+    {
+      { "<C-h>", "<cmd> TmuxNavigateLeft<CR>", desc = "window left" },
+      { "<C-l>", "<cmd> TmuxNavigateRight<CR>", desc = "window right" },
+      { "<C-j>", "<cmd> TmuxNavigateDown<CR>", desc = "window down" },
+      { "<C-k>", "<cmd> TmuxNavigateUp<CR>", desc = "window up" },
+      { "<leader>gg", "<cmd>LazyGit <CR>", desc = "Lazygit" },
+      -- {"<leader>tt", function () require("trouble").toggle() end, desc = "Toggle LSP diagnostics"},
+      -- {"<leader>tw", function () require("trouble").toggle("workspace_diagnostics") end, desc = "LSP worksace diagnostics"},
+      -- {"<leader>td", function () require("trouble").toggle("diagnostics_buffer") end, desc = "LSP document diagnostics"},
+      -- {"<leader>tq", function () require("trouble").toggle("quickfix") end, desc = "LSP quickfix items"},
+      -- {"<leader>tl", function () require("trouble").toggle("loclist") end, desc = "LSP location list items"},
+      -- {"<leader>gR", function () require("trouble").toggle("lsp_references") end, desc = "LSP references of word under cursor"},
+      {
+        "<leader>pvs",
+        function()
+          require("swenv.api").get_current_venv()
+        end,
+        desc = "Show the current venv",
+      },
+      {
+        "<leader>pvp",
+        function()
+          require("swenv.api").pick_venv()
+        end,
+        desc = "Pick a venv",
+      },
+      {
+        "<leader>trf",
+        function()
+          require("neotest").run.run(vim.fn.expand "%")
+        end,
+        desc = "Run tests in file",
+      },
+      {
+        "<leader>trn",
+        function()
+          require("neotest").run.run()
+        end,
+        desc = "Run nearest test",
+      },
+      {
+        "<leader>ls",
+        function()
+          vim.lsp.buf.signature_help()
+        end,
+        desc = "LSP signature help",
+      },
+      {
+        "<leader>lf",
+        function()
+          vim.diagnostic.open_float { border = "rounded" }
+        end,
+        desc = "Floating diagnostic",
+      },
+      {
+        "<leader>ra",
+        function()
+          require("nvchad.renamer").open()
+        end,
+        desc = "LSP rename",
+      },
+
+      {
+        "<leader>ca",
+        function()
+          vim.lsp.buf.code_action()
+        end,
+        desc = "LSP code action",
+      },
+    },
+  },
+}
